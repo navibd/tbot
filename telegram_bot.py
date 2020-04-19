d8f46878d13404ace0acacd8f1177cc8b292c1f8
navibd <navibd@gmail.com>
2020-04-19 09:11:03 -0700

    first commit

--- telegram_bot.py
+++ telegram_bot.py
@@ -12,7 +12,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def start(bot, update):
 
-    update.effective_message.reply_text("Hi!!!")
+    update.effective_message.reply_text("Hi!")
 
 
 
@@ -37,13 +37,12 @@ if __name__ == "__main__":
     # Set these variable to the appropriate values
 
     TOKEN = "934772110:AAEZlF71gtR_jBL2V4kEjdOy09QJHYWqC2A"
-    NAME  = "ttbot001"
+
+    NAME = "ttbot001"
 
 
 
-    # Port is given by Heroku
-
-    PORT = os.environ.get('PORT')
+    # Port is givet = os.environ.get('PORT')
 
 
 
@@ -67,4 +66,20 @@ if __name__ == "__main__":
 
     dp.add_handler(CommandHandler('start', start))
 
-    dp.add_handler(MessageHandler(Filters.te
+      dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+
+
+    # Start the webhook
+
+    updater.start_webhook(listen="0.0.0.0",
+
+                          port=int(PORT),
+
+                          url_path=TOKEN)
+
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+
+    updater.idle()
